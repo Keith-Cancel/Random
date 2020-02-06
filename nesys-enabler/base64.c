52c988bdec5c6a720ade40cc80c7a7c944619362
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2020-02-06 06:29:55 -0800

    Messed up a copy and paste.

--- nesys-enabler/base64.c
+++ nesys-enabler/base64.c
@@ -72,33 +72,9 @@ void to_base64_no_buffer_check(const uint8_t* data, size_t len, uint8_t* buf) {
     size_t in_index  = 0;
     size_t out_index = 0;
     while(in_index <= (len - 4)) {
-    static const char tbl[] = {
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-        'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-        'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
-        'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-        'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-        'w', 'x', 'y', 'z', '0', '1', '2', '3',
-        '4', '5', '6', '7', '8', '9', '+', '/'
-    };
-    size_t in_index  = 0;
-    size_t out_index = 0;
-    while(in_index <= (len - 4)) {
         uint32_t val;
         memcpy(&val, data + in_index, 4);
         val = is_litte_endian() ? byte_swap32(val) : val;
-        // Breaking this up as consts some reason
-        // makes clang spill more registers
-        /*const uint8_t i_0 = (val >>  8) & 0x3f;
-        const uint8_t i_1 = (val >> 14) & 0x3f;
-        const uint8_t i_2 = (val >> 20) & 0x3f;
-        const uint8_t i_3 = (val >> 26);
-        uint32_t out;
-        out  = tbl[i_0] << (is_litte_endian() ? 24 : 0);
-        out |= tbl[i_1] << (is_litte_endian() ? 16 : 8);
-        out |= tbl[i_2] << (is_litte_endian() ? 8 : 16);
-        out |= tbl[i_3] << (is_litte_endian() ? 0 : 24);*/
         uint32_t out;
         out  = tbl[(val >>  8) & 0x3f] << (is_litte_endian() ? 24 : 0);
         out |= tbl[(val >> 14) & 0x3f] << (is_litte_endian() ? 16 : 8);
