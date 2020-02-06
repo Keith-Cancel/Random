a40c9813009b0e3374e3f0a3d4ff88f3ad5fc487
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2020-02-06 05:54:37 -0800

    Optimize a little
    Made the last 1-3 bytes not branch for the fun of it.
    Made the main loop more efficient, probably still could be improved.

--- nesys-enabler/base64.c
+++ nesys-enabler/base64.c
@@ -19,6 +19,30 @@ SOFTWARE.
 */
 #include "base64.h"
 
+#define FULL_GCC_VERSION (__GNUC__ * 100 + __GNUC_MINOR__)
+// Clang builtin check
+#ifndef __has_builtin
+#  define __has_builtin(x) 0
+#endif
+
+#if FULL_GCC_VERSION > 402 || __has_builtin(__builtin_bswap32)
+#define byte_swap32 __builtin_bswap32
+#else
+#define ROTL32(x, a)   (((x) << (a)) | ((x) >> (32 - (a))))
+static uint32_t byte_swap32(uint32_t value) {
+    return (ROTR32(value, 8) & 0xff00ff00) |
+           (ROTL32(value, 8) & 0x00ff00ff);
+}
+#endif
+
+static inline bool is_litte_endian(void) {
+    union {
+        uint32_t v;
+        uint8_t  b[4];
+    } chk = {0x01000000};
+    return chk.b[0] != 1;
+}
+
 size_t get_base64_length(size_t length) {
     size_t x   = length * 4;
     size_t j   = x / 3 + ((x % 3) > 0);
@@ -48,36 +72,55 @@ void to_base64_no_buffer_check(const uint8_t* data, size_t len, uint8_t* buf) {
     size_t in_index  = 0;
     size_t out_index = 0;
     while(in_index <= (len - 4)) {
-        uint8_t val[4];
+    static const char tbl[] = {
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+        'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+        'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+        'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+        'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+        'w', 'x', 'y', 'z', '0', '1', '2', '3',
+        '4', '5', '6', '7', '8', '9', '+', '/'
+    };
+    size_t in_index  = 0;
+    size_t out_index = 0;
+    while(in_index <= (len - 4)) {
+        uint32_t val;
         memcpy(&val, data + in_index, 4);
-        const uint8_t i_0 =   val[0] >> 2;
-        const uint8_t i_1 = ((val[0] << 4) | (val[1] >> 4)) & 0x3f;
-        const uint8_t i_2 = ((val[1] << 2) | (val[2] >> 6)) & 0x3f;
-        const uint8_t i_3 =   val[2] & 0x3f;
-        uint8_t out[4] = {
-                tbl[i_0], tbl[i_1], tbl[i_2], tbl[i_3]
-        };
+        val = is_litte_endian() ? byte_swap32(val) : val;
+        // Breaking this up as consts some reason
+        // makes clang spill more registers
+        /*const uint8_t i_0 = (val >>  8) & 0x3f;
+        const uint8_t i_1 = (val >> 14) & 0x3f;
+        const uint8_t i_2 = (val >> 20) & 0x3f;
+        const uint8_t i_3 = (val >> 26);
+        uint32_t out;
+        out  = tbl[i_0] << (is_litte_endian() ? 24 : 0);
+        out |= tbl[i_1] << (is_litte_endian() ? 16 : 8);
+        out |= tbl[i_2] << (is_litte_endian() ? 8 : 16);
+        out |= tbl[i_3] << (is_litte_endian() ? 0 : 24);*/
+        uint32_t out;
+        out  = tbl[(val >>  8) & 0x3f] << (is_litte_endian() ? 24 : 0);
+        out |= tbl[(val >> 14) & 0x3f] << (is_litte_endian() ? 16 : 8);
+        out |= tbl[(val >> 20) & 0x3f] << (is_litte_endian() ? 8 : 16);
+        out |= tbl[(val >> 26)] << (is_litte_endian() ? 0 : 24);
         memcpy(buf + out_index, &out, 4);
         in_index  += 3;
         out_index += 4;
     }
-    // Handle the last 1-3 bytes
-    uint32_t bits  = 0;
-    unsigned shift = 6;
-    for(;in_index < len; in_index++) {
-        uint8_t byte = data[in_index];
-        bits  |= byte >> (8 - shift);
-        buf[out_index] = tbl[bits];
-        bits >>= 6;
-        // add the remaining bits.
-        bits  |= (0xff >> shift) & byte;
-        // adjust shift
-        shift -= 2;
-        bits <<= shift;
-        out_index++;
-    }
-    unsigned x = (~out_index) & 0x3;
-    buf[out_index + (x >> 1)] = '=';
-    buf[out_index + x]        = '=';
-    buf[out_index] = tbl[bits];
+    // Handle the last 1-3 bytes without branches XD
+    // Did more for fun than anything
+    buf  += out_index;
+    data += in_index;
+    const unsigned rem   = len - in_index;
+    const unsigned rem_s = rem >> 1;
+    uint32_t bits = data[rem - 1] * (rem & rem_s);
+    bits |= ((uint32_t)data[rem_s] * rem_s) << 8;
+    bits |= ((uint32_t)data[0])             << 16;
+    buf[3]        = '=';
+    buf[2]        = '=';
+    buf[rem]      = tbl[ bits & 0x3f];
+    buf[rem_s + 1]= tbl[(bits >>  6) & 0x3f];
+    buf[1]        = tbl[(bits >> 12) & 0x3f];
+    buf[0]        = tbl[ bits >> 18];
 }
